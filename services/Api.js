c6e1369697d5669b3fa0e4176be65817ce91f73c
MarcoBz <barison.marco.91@gmail.com>
2019-06-09 16:29:42 +0200

    fixed error in app url

--- services/Api.js
+++ services/Api.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 const port = process.env.PORT || 4000
+const url = "https://protected-mesa-49093.herokuapp.com"
 export default() => {
   return axios.create({
-    baseURL: `http://localhost:` + port + '/api'
+    //baseURL: `http://localhost:` + port + '/api'
+    baseURL: url + '/api'
   })
 }
